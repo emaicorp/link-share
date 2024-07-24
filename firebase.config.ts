7d3c396e0ff39ac8cd857aca5879c8db7724d21e
emaicorp <emaicorp@gmail.com>
2024-07-24 23:31:28 +0100

    fixed api issue

--- firebase.config.ts
+++ firebase.config.ts
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "link-share-3f8cf.firebaseapp.com",
-  projectId: "link-share-3f8cf",
-  storageBucket: "link-share-3f8cf.appspot.com",
-  messagingSenderId: "516695803802",
-  appId: process.env.APP_ID,
-  measurementId: "G-THD44WV8D7"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
